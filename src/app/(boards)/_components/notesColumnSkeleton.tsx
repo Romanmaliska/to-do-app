94df44d477a93bebd2b2f96c7f68c5825354d24f
Roman Maliska <roman.maliska@firma.seznam.cz>
2025-01-10 13:45:14 +0100

    feat: notes column skeleton

--- src/app/(boards)/_components/notesColumnSkeleton.tsx
+++ src/app/(boards)/_components/notesColumnSkeleton.tsx
@@ -9,18 +9,20 @@ type Props = {
 
 export default function NotesColumnSkeleton({ draggedColumn }: Props) {
   return (
-    <div className='flex flex-col w-52 h-16 rounded-xl bg-grey p-2'>
-      <h2>{draggedColumn.columnTitle}</h2>
+    <div className='flex flex-col w-52 h-fit rounded-xl bg-grey p-2'>
+      <h1 className='min-w-32 rounded-md h-9 p-2 pl-4 mb-2'>
+        {draggedColumn.columnTitle}
+      </h1>
       {draggedColumn.notes.map((note) => (
         <p
+          className='h-9 m-1 px-4 py-2 bg-white border-0 rounded-md'
           key={note.noteId}
-          className='h-9 m-1 px-4 py-2 bg-white border-0 rounded-md transform -rotate-12'
         >
           {note.noteText}
         </p>
       ))}
       <Button
-        className='flex items-center justify-start gap-1 mt-1 h-9 pl-3 hover:bg-darkGrey'
+        className='flex items-center justify-start gap-1 mt-1 h-9 pl-3 bg-grey p-2'
         variant='ghost'
       >
         <BsPlus size={18} /> Add a note
